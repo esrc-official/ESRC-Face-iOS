19fb3a9504baa6a0b67a8169ccb70bb1967bfeba
lhw4846 <lhw4846@esrc.co.kr>
2021-10-29 11:41:16 +0900

    Initial Commit

---
+++ ESRC-Face-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ESRC-Face-iOS
+//
+//  Created by Mac_Joseph on 2021/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
